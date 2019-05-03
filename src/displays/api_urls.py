9f0cfcc95589bbd4302d7fdbeb9def3d830de5e2
Patrick van der Tuin <patricktuin78@gmail.com>
2019-05-04 01:50:18 +0200

    NEW - Removing params.py in gitignore

--- src/displays/api_urls.py
+++ src/displays/api_urls.py
@@ -1,10 +1,10 @@
-from django.urls import path, include
+from django.urls import path
 from rest_framework import routers
 
 from displays.api_views import DisplayDetailAPIView
 
 router = routers.DefaultRouter()
-# router.register(r'', DisplayDetailAPIView)
+
 
 urlpatterns = [
     path('get_serial/<int:serial_number>', DisplayDetailAPIView.as_view()),
