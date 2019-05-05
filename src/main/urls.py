b658f35aeb87d30e0844c87cdde12e3d8bf6284a
Patrick van der Tuin <patricktuin78@gmail.com>
2019-05-05 12:53:58 +0200

    NEW - Adding empty url pattern for index page

--- src/main/urls.py
+++ src/main/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('display/', include('displays.urls', namespace='display')),
     path('accounts/', include('accounts.urls', namespace='accounts')),
     path('api/', include('displays.api_urls')),
+    path('', include('accounts.urls', namespace='accounts')),
 ]
